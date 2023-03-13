e5afe04303869453431abd8a1132d53776560b27
turtle5996 <125853038+turtle5996@users.noreply.github.com>
2023-03-13 15:54:54 +0900

    Baseapp

---
+++ MainActivity.java
@@ -0,0 +1,40 @@
+package com.cookandriod.baseapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button button1;
+    Button button2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher);
+
+
+        button1 = (Button) findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "학번 : 20190831",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        button2 = (Button) findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "이름 : 신지섭",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
